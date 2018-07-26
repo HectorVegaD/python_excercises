040bd943dcd785561b8cddcc786dfcc1a92e5ff8
Hector De La Vega <hectordelavegak@gmail.com>
2018-07-26 16:54:14 -0700

    My First Commit

---
+++ ch2_messages.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  5 12:35:24 2018
+
+@author: hectordelavega
+"""
+
+message = "Hello World!"
+print(message)
+
+message = "Work is necessary for change, Hector."
+message1 = 'Viridiana Prado'
+print(message)
+
+print('Hello "'+ message + '" How is ' + message1)
+print(message, message1)
+
+
+# The Number 8 (Addition, Subtraction, Multiplication, Division Resulting in 8)
+print(5 + 3)
+print(10 - 2)
+print(4 * 2)
+print(64 / 8)
+
+# Favorite Number
+fav_num = 7
+message = 'My favorite number is: ' + str(fav_num)
+print(message)
